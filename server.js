04bc300b767eaf61b73b46ec4e4709daaad57d22
elifozker <elif_ozker@hotmail.com>
2023-09-02 14:52:42 +0300

    product model and post operation

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const Product = require('./models/productModel');
 const app = express()
 const port = 3000
+
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Node API listening on port ${port}`)
+app.post('/product',async(req,res) => {
+  try{
+    const product = await Product.create(req.body)
+    res.status(200).json(product);
+
+  }catch(error){
+    console.log(error);
+    res.status(500).json({message: error.message})
+  }
 })
+
+
+mongoose.connect('mongodb+srv://elifozker:Uykusuz01@cluster0.ba3zr3y.mongodb.net/crudsimple?retryWrites=true&w=majority').then(() =>{
+  console.log("Connected to mongodb");
+  app.listen(port, () => {
+    console.log(`Node API listening on port ${port}`)
+  });
+}).catch(() =>{
+  console.log(error);
+});
